ef514ac1cd5d1c00abd1feff5a219bf73084f3c8
Teddy2024 <teddy31120@gmail.com>
2022-12-27 23:25:41 +0800

    123

--- R20-3D/Assets/HomeWork/Scripts/DialogueSystem.cs
+++ R20-3D/Assets/HomeWork/Scripts/DialogueSystem.cs
@@ -10,9 +10,8 @@ namespace Teddy
         #region 資料區域
         [SerializeField, Header("對話間格"), Range(0, 0.5f)]
         private float dialogueIntervalTime = 0.1f;
-
         [SerializeField, Header("開頭對話")]
-        private DialogueData  dialogueOpening;
+        private DialogueData dialogueOpening;
 
         private WaitForSeconds dialogueInterval => new WaitForSeconds(dialogueIntervalTime);
         //prop(屬性欄)看不懂,LAMBDA=>也看不懂
@@ -30,9 +29,27 @@ namespace Teddy
             textContent = GameObject.Find("TalkSomething").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("TalkFinishIcon");
             goTriangle.SetActive(false);
+
+            //啟動協程
+            StartCoroutine(FadeGroup());
         }    
         #endregion
         
+        private IEnumerator FadeGroup()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+        private IEnumerator TyperEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+            string dialogue = dialogueOpening.dialogueContents[0];
+            yield return dialogueIntervalTime;
+        }
     }
 }
 
