2e493d0b8648ba9f7d5399ab2c899bcb5007189e
Teddy2024 <teddy31120@gmail.com>
2022-12-27 22:25:57 +0800

    Update LearnCoroutine.cs

--- R20-3D/Assets/HomeWork/Scripts/LearnCoroutine.cs
+++ R20-3D/Assets/HomeWork/Scripts/LearnCoroutine.cs
@@ -1,18 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LearnCoroutine : MonoBehaviour
+namespace Teddy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LearnCoroutine : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
+
