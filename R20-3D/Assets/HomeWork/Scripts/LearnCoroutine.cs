182a0fe87feb78681d469179c46b86d89167e9f9
Teddy2024 <teddy31120@gmail.com>
2022-12-27 22:50:29 +0800

    Update LearnCoroutine.cs

--- R20-3D/Assets/HomeWork/Scripts/LearnCoroutine.cs
+++ R20-3D/Assets/HomeWork/Scripts/LearnCoroutine.cs
@@ -14,12 +14,33 @@ namespace Teddy
         //方法內 yield return(等待)
         //使用StartCoroutine(啟動)
 
+        private string testDialogue = "亞洲大學好恐怖";
+
+        private void Awake() 
+        {
+            StartCoroutine(Test());
+            Debug.Log(testDialogue[0]);
+            StartCoroutine(ShowDialogue());
+        }
+
+        #region Test
         private IEnumerator Test()
         {
             Debug.Log("First");
             yield return new WaitForSeconds(2);
             Debug.Log("Second");
-        }
+            yield return new WaitForSeconds(3);
+            Debug.Log("Third");
+        }    
+        #endregion
+        private IEnumerator ShowDialogue()
+        {
+            Debug.Log(testDialogue[0]);
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log(testDialogue[1]);
+            yield return new WaitForSeconds(0.1f);
+            Debug.Log(testDialogue[2]);
+        }    
     }
 }
 
