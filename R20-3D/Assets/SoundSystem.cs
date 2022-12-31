86dcd5b25f5ea3b323a322ffeb79e4ef18f4e257
Teddy2024 <teddy31120@gmail.com>
2022-12-31 13:16:56 +0800

    1

---
+++ R20-3D/Assets/SoundSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Teddy
+{
+    [RequireComponent(typeof(AudioSource))]
+
+    public class SoundSystem : MonoBehaviour
+    {
+        private void Awake() 
+        {
+        
+        }
+    }
+}
+
+
